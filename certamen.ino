a8a648f54fd2164d978dbcc9578633245fc4411b
arpruss <arpruss@gmail.com>
2017-05-10 11:30:55 -0500

    comments

--- certamen.ino
+++ certamen.ino
@@ -1,27 +1,6 @@
-/*
-  Button
-
- Turns on and off a light emitting diode(LED) connected to digital
- pin 13, when pressing a pushbutton attached to pin 2.
-
-
- The circuit:
- * LED attached from pin 13 to ground
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
-
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
-
-
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/Button
+/* Code (c) 2017 Alexander Pruss.
+ *  
+ *  Uses some public domain stuff from http://www.arduino.cc/en/Tutorial/Button
  */
 
 #define TEST_MODE 
