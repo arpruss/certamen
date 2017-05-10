5380977ca84efe7d73b078ae7a6246c3544a1130
arpruss <arpruss@gmail.com>
2017-05-10 15:13:24 -0500

    fixes

--- certamen.ino
+++ certamen.ino
@@ -4,14 +4,23 @@
  */
 
 #define TEST_MODE 
-
+#define RANDOM_PRESS_FREQUENCY 0
+#define NUMBER_OF_RANDOM_PRESSES 2
 #define BUTTON_MODE INPUT_PULLUP // TODO: change for final version
+#define OSCILLATION_TEST 1
+#undef TEST_MODE_PUSHBUTTON
 
 typedef unsigned char uint8;
 typedef signed char sint8;
 
+#ifdef TEST_MODE
 const uint8 testPin = 53;
+#ifdef OSCILLATION_TEST
+uint8 testPinState = 0;
+#endif
+#endif
 
+const uint8 unconnectedAnalogPin = 5;
 const uint8 debounceTime = 50;
 // constants won't change. They're used here to
 // set pin numbers:
@@ -20,25 +29,26 @@ const uint8 ledPin =  13;      // the number of the LED pin
 const uint8 buzzerPin = 9;
 const uint8 numTeams = 3;
 const uint8 playersPerTeam = 4;
-const uint8 playerPins[numTeams][playersPerTeam] = {
-{30,31,32,33},
-{34,35,36,37},
-{38,39,40,41}
+const uint8 playerPins[numTeams*playersPerTeam] = {
+30,31,32,33,
+34,35,36,37,
+38,39,40,41
 };
-const uint clearPin = 42;
-const uint certamenModePin = 3;
+const uint8 clearPin = 42;
+const uint8 certamenModePin = 3;
 
-// variables will change:
+char tonePlaying = 0;
 char buttonState = 0;         // variable for reading the pushbutton status
 char ledState = 0;
 long lastButtonDownTime = -1;
-long toneOffTime = -1;
+long toneOffTime = 0;
 uint8 pressOrder[numTeams*playersPerTeam];
 char buttonDown[numTeams*playersPerTeam];
 char teamButtonDown[numTeams];
 uint8 numPressed = 0;
 char certamenMode = 1;
 sint8 add[numTeams*playersPerTeam]; // maximum of 128 players
+char stringBuffer[128];
 
 inline uint8 getPlayerID(uint8 team, uint8 player) {
   return team*playersPerTeam + player;
@@ -46,6 +56,10 @@ inline uint8 getPlayerID(uint8 team, uint8 player) {
 
 inline int getTeamFromID(uint8 id) {
   return id/playersPerTeam;
+}
+
+inline int getPlayerFromID(uint8 id) {
+  return id % playersPerTeam;
 }
 
 void clearState(char force) {
@@ -57,12 +71,20 @@ void clearState(char force) {
     Serial.println("Clear");
 #endif
   }
-#ifdef TEST_MODE
+#if defined(TEST_MODE) && !defined(OSCILLATION_TEST)
   digitalWrite(testPin, LOW);
 #endif
 }
 
+char* describePlayerID(uint8 id) {
+  uint8 team = getTeamFromID(id);
+  uint8 player = getPlayerFromID(id);
+  sprintf(stringBuffer, "%c%d", 'A'+team,player+1);
+  return stringBuffer;
+}
+
 void setup() {
+  randomSeed(analogRead(unconnectedAnalogPin));
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
   pinMode(clearPin, INPUT_PULLUP);
@@ -70,8 +92,8 @@ void setup() {
   pinMode(buzzerPin, OUTPUT);
   pinMode(testPin, OUTPUT);
   digitalWrite(ledPin, LOW);
-  for (int i=0; i<numTeams; i++) for (int j=0; j<playersPerTeam; j++) {
-    pinMode(playerPins[i][j], BUTTON_MODE); // for testing
+  for (int id=0; id<numTeams*playersPerTeam; id++) {
+    pinMode(playerPins[id], BUTTON_MODE); 
     //TODO: disable failed?
   }
   clearState(1);
@@ -82,20 +104,34 @@ void setup() {
 }
 
 void scan() {
+#if defined(TEST_MODE) && defined(OSCILLATION_TEST)
+  testPinState = ! testPinState;
+  digitalWrite(testPin, testPinState);
+#endif
+
   uint8 toAdd = 0;
   uint8 previousNumPressed = numPressed;
-  
-  for (uint8 i=0; i<numTeams; i++) {
-    if (!certamenMode || !teamButtonDown[i])
-      for (uint8 j=0; j<playersPerTeam; j++) {
-        uint8 id = getPlayerID(i,j);
-        if (!buttonDown[id] && LOW == digitalRead(playerPins[i][j])) {
+
+  for (int id=numTeams*playersPerTeam-1; id>=0; id--) {
+      if (LOW == digitalRead(playerPins[id]) && !buttonDown[id] && (!certamenMode || !teamButtonDown[getTeamFromID(id)])) {
           buttonDown[id] = 1;
-          add[toAdd] = id;
-          toAdd++;
-        }
+          add[toAdd++] = id;
       }
   }
+
+#if defined(TEST_MODE) && RANDOM_PRESS_FREQUENCY>0
+  if (toAdd == 0 && random(RANDOM_PRESS_FREQUENCY) == 0) {
+//    Serial.println("Random presses");
+//    Serial.println(String("Teams ") + (teamButtonDown[0]?"yes ":"no ") +(teamButtonDown[1]?"yes ":"no ")+(teamButtonDown[2]?"yes":"no"));
+    for (uint8 i = 0; i < NUMBER_OF_RANDOM_PRESSES ; i++) {
+      uint8 id = random(numTeams*playersPerTeam);
+      if (!buttonDown[id] && (!certamenMode || !teamButtonDown[getTeamFromID(id)] ) ) {
+        buttonDown[id] = 1;
+        add[toAdd++] = id;
+      }
+    }
+  }
+#endif  
 
   if (toAdd) {
     if (toAdd == 1) { /* the usual, optimized case */
@@ -105,38 +141,54 @@ void scan() {
     else {
       /* randomize simultaneous presses */
       /* TODO: test this in software, as it so rarely triggers */
-      for (uint8 i=0; i<toAdd; i++) {
-        uint8 n = random() % (toAdd-i);
-        for (uint8 j=0; j<toAdd; j++) {
+      sint8 leftToAdd = toAdd;
+      uint8 j;
+      do {
+        uint8 n = random() % leftToAdd;
+        for (j=0; j<toAdd; j++) {
           if (add[j] > -1) {
             if (n==0) {
               uint8 id = add[j];
-              teamButtonDown[getTeamFromID(id)] = 1;
+              uint8 team = getTeamFromID(id);
+              teamButtonDown[team] = 1;
               pressOrder[numPressed++] = id;
-              add[j] = -1;
+              if (certamenMode) {
+                for (uint8 k=0; k<toAdd; k++) {
+                  id = add[k];
+                  if (id > -1 && getTeamFromID(id) == team) {
+                    add[k] = -1;
+                    leftToAdd--;
+                  }
+                }
+              }
+              else {
+                add[j] = -1;
+                leftToAdd--;
+              }
               break;
             }
             n--;
           }
         }
-      }
+      } while(leftToAdd > 0 && j<toAdd); // the second condition should never be satisfied
     }
 
-#ifdef TEST_MODE
+#if defined(TEST_MODE) && !defined(OSCILLATION_TEST)
     digitalWrite(testPin, HIGH);
 #endif
 
     if (previousNumPressed == 0) {
         tone(buzzerPin, 800);
         toneOffTime = millis() + 500;
+        tonePlaying = 1;
 #ifdef TEST_MODE
         Serial.println("buzz");
 #endif
     }
 #ifdef TEST_MODE
-    Serial.println("pressed: ");
+    Serial.println(certamenMode ? "pressed in certamen mode:" : "pressed in non-certamen mode:");
     for (uint8 i=0; i<numPressed;i++) {
-      Serial.println(""+String(pressOrder[i]));
+      Serial.println(describePlayerID(pressOrder[i]));
     }
 #endif
     /* TODO: display */
@@ -144,12 +196,12 @@ void scan() {
 }
 
 void loop() {
-  if (toneOffTime != -1 && millis() >= toneOffTime) {
+  if (tonePlaying && millis() >= toneOffTime) {
     noTone(buzzerPin);
-    toneOffTime = -1;
+    tonePlaying = 0;
   }
   
-#ifdef TEST_MODE
+#ifdef TEST_MODE_PUSHBUTTON
   // read the state of the pushbutton value
   uint8 currentButton = digitalRead(buttonPin);
 
@@ -162,6 +214,7 @@ void loop() {
         digitalWrite(ledPin, ledState ? HIGH : LOW); // LED state
         tone(buzzerPin, 800);
         toneOffTime = millis() + 500;
+        tonePlaying = 1;
       }
       lastButtonDownTime = millis();
   } else {
@@ -176,6 +229,8 @@ void loop() {
   char certamenModeSwitch = HIGH == digitalRead(certamenModePin);
 
   if (certamenModeSwitch != certamenMode) {
+    // TODO: might want to debounce this a little to avoid too many calls to clearState(),
+    // but is it worth the bother? Well, it might depend on how the display works.
     clearState(0);
     certamenMode = certamenModeSwitch;
   } 
